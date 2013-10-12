261e30d296efed290e983c6282318a48ba2ff45c
Shervin Shaikh <shervin.shaikh@appfolio.com>
2013-10-12 14:06:13 -0700

    ss/rs/rr - Order by score and limit to 20 persons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,11 @@
 class HomeController < ApplicationController
 	def index
-
 		@user = User.first
 
 		if !session[:user_id].nil?
 			@u = User.find(session[:user_id])
-			@topHipsters = @u.friends.order(:score)
+			@topHipsters = @u.friends.order('score desc').limit(20)
 		end
-
-
-	end
-
-	def facebook_auth
-		# @graph = Koala::Facebook::API.new
 	end
 
 end
