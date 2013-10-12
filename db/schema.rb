8b97a8072b168bb7539044c2d8f157d71de578d0
Johan Baversjo <baversjo@gmail.com>
2013-10-12 01:46:09 -0700

    friend model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012063743) do
+ActiveRecord::Schema.define(version: 20131012084229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: true do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
