649c04bf6d94070fabc6c6da7d874f8035d06f5d
Johan Baversjo <baversjo@gmail.com>
2013-10-11 21:37:32 -0700

    secret token fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thmc::Application.config.secret_key_base = 'c778caf9e85aad5517e231a8d7477379d439f3019e8000d7ccd81f8c8863a00c7caefbfb32e42ee8c5b9ceb8b3474443f97559dc64c334e05cf5074d9eda6383'
